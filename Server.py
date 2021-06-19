21489bd415be2f59f09f8de9b263b71d2bbbda52
n1c0201 <nicarthur21@gmail.com>
2021-06-19 13:56:35 +0700

    created ChatThread class

--- Server.py
+++ Server.py
@@ -1,2 +1,16 @@
 from socket import *
 from threading import *
+
+class ChatThread(Thread):
+    def __init__(self, con):
+        Thread.__init__(self)
+        self.con = con
+    def run(self):
+        name = current_thread().getName()
+        while True:
+            if name == 'Sender':
+                data=input('Server:')
+                self.con.send(bytes(data, 'utf-8'))
+            elif name == 'Receiver':
+                recData = self.con.recv(1024).decode()
+                print('Client: ', recData)
