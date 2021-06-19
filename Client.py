d90eeaab383523ca5931825625522299b2db65d1
jeremyponto <56902787+jeremyponto@users.noreply.github.com>
2021-06-19 17:11:16 +0800

    Improved server and client scripts

--- Client.py
+++ Client.py
@@ -1,2 +1,35 @@
-from socket import *
-from threading import *
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 19 16:09:57 2021
+
+@author: jerem
+"""
+
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDRESS = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDRESS)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    message_length = len(message)
+    send_length = str(message_length).encode(FORMAT)
+    send_length += b" " * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("Hello World!")
+input()
+send("Hello Everyone!")
+input()
+send("Hello Jeremy!")
+
+send(DISCONNECT_MESSAGE)
--- Server.py
+++ Server.py
@@ -1,2 +1,48 @@
-from socket import *
-from threading import *
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 19 15:13:02 2021
+
+@author: jerem
+"""
+
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDRESS = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDRESS) 
+
+def handle_client(connection, address):
+    print(f"[NEW CONNECTION] {address} connected.")
+    
+    connected = True
+    while connected:
+        message_length = connection.recv(HEADER).decode(FORMAT)
+        if message_length:
+            message_length = int(message_length)
+            message = connection.recv(message_length).decode(FORMAT)
+            if message == DISCONNECT_MESSAGE:
+                connected = False
+        
+        print(f"[{address}] {message}")
+        connection.send("Message received".encode(FORMAT))
+    
+    connection.close()
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        connection, address = server.accept()
+        thread = threading.Thread(target = handle_client, args = (connection, address))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+print("[STARTING] Starting server...")
+start()
